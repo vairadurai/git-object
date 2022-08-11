a572ce3c3cab307b3edfc69ad704e505ad1e661e
vairadurai <vairadurai007@gmail.com>
2022-08-11 17:08:21 +0530

    add

---
+++ main.js
@@ -0,0 +1,98 @@
+
+
+
+
+
+// OBJECTS
+
+
+
+//1.phone
+let phone ={brand:"realme",ram:"4gp",size:"6 inch",camera:"16bp,12mp"};
+
+//2.laptop
+let laptop ={brand:"lenovo",ram:"4gp",size:"1200px",battery:"300amps"};
+
+//3.Note book
+let notebook ={brand:"diamond",model:"ruled",pages:"152",price:"50"};
+
+//4. watch
+let watch ={brand:"rolex",made:"gold",price:"1lack",type:"digital"};
+
+//5, cricket bat
+let bat ={brand:"MRF",model:"lethaer",made:"english willow",price:"7,500"};
+
+//6. cricket ball
+let ball ={brand:"truth trust",model:"stich ball",made:"leather",}
+
+//7.head phone
+let headphone ={brand:"sony",model:"bluetooth",battery:"1000amps",price:"1000"};
+
+//8.fan
+let fan ={brand:"crompton",wings:"3",speed:"1200rpm",price:"3500"};
+
+//9.shirt
+let shirt ={brand:"OTTO",model:"off hand",colour:"green",price:"2000"};
+
+//10.pant
+let pant={brand:"raymond",size:"XXL",colour:"yellow",price:"3000"};
+
+//11.charger
+let charger={brand:"vooc",type:"c",watts:"15",colour:"white",price:"4000"};
+
+//12.bike
+let bike={brand:"yamaha",model:"Rl5",colour:"black",price:"200000"};
+
+//13.cycle
+let cycle={brand:"herculs",type:"tube less",colour:"green",price:"5000"};
+
+//14.tv
+let tv={brand:"sony",size:"15inch",colour:"black",price:"15000"};
+
+//15.pencil
+let pencil={brand:"absara",model:"HB",colour:"black-white",price:"5"};
+
+//16.shoe
+let shoe={brand:"addidas",size:"10",colour:"white",price:"600"};
+
+//17.chappal
+let chappal={brand:"nike",model:"woodland",size:"10",price:"3000"};
+
+//18.watter bottel
+let watterbottel={brand:"bisleri",quantity:"one litter",price:"20",type:"filter"};
+
+//19.T-shirt
+let Tshirt={brand:"bizzo",size:"XL",colour:"red",price:"500"};
+
+//20.bag
+let bag={brand:"skybag",model:"water-broof",colour:"yellow",price:"600"};
+
+//21.bulb
+let bulb={brand:"philips",watts:"24v",colour:"white",price:"60"};
+
+//22.battery
+let battery={brand:"premio",volt:"50volt",price:"8500",weight:"2kg"};
+
+//23.chocolate
+let chocolate={brand:"dairy-milk",type:"padtham",colour:"brown",price:"10"}
+
+//24.actor
+let actor={name:"karthic",weight:"75",colour:"brown",favorite:"dance"};
+
+//25.ring
+let ring={brand:"jos alukas",type:"round",made:"gold",price:"137800"};
+
+//26.ice creme
+let icecreme={brand:"arun",type:"vennila",made:"chocolate",price:"50"};
+
+//27.lipstic
+let lipstic={brand:"foxy",colour:"rose",quantity:"50ml",price:"100"};
+
+//28.sun glass
+let sunglass={brand:"galvin",colour:"blue",model:"RB 3025",price:"5000"};
+
+//29.beer
+let beer={brand:"Amsteel",type:"leather",limit:"8%",price:"160"};
+
+//30.chekken
+let chiken={brand:"suguna",type:"brailer",colour:"white",price:"210"};
